48bda381416de3860953d95dcd3f88da904ed4a9
Adam Derylo <aderylo@noreply>
2025-11-23 07:02:14 +0100

    Clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
     import { appState } from "$lib/stores/appState";
-    import LeftSidebar from "$lib/components/LeftSidebar.svelte";
-    import RightSidebar from "$lib/components/RightSidebar.svelte";
-    import BigPictureView from "$lib/components/BigPictureView.svelte";
-    import YearlyView from "$lib/components/YearlyView.svelte";
-    import MonthlyView from "$lib/components/MonthlyView.svelte";
-    import WeeklyView from "$lib/components/WeeklyView.svelte";
-    import CalendarView from "$lib/components/CalendarView.svelte";
     import RetroIntroView from "$lib/components/RetroIntroView.svelte";
     import { fade } from "svelte/transition";
 
@@ -15,35 +8,9 @@
 </script>
 
 <main>
-    {#if mode === "intro"}
-        <div class="fullscreen-center" in:fade>
-            <RetroIntroView />
-        </div>
-    {:else}
-        <div class="dashboard-layout" in:fade>
-            <LeftSidebar />
-
-            <div class="center-stage">
-                {#key currentView}
-                    <div class="view-container" in:fade={{ duration: 300 }}>
-                        {#if currentView === "big-picture"}
-                            <BigPictureView />
-                        {:else if currentView === "yearly"}
-                            <YearlyView />
-                        {:else if currentView === "monthly"}
-                            <MonthlyView />
-                        {:else if currentView === "weekly"}
-                            <WeeklyView />
-                        {:else if currentView === "calendar"}
-                            <CalendarView />
-                        {/if}
-                    </div>
-                {/key}
-            </div>
-
-            <RightSidebar />
-        </div>
-    {/if}
+    <div class="fullscreen-center" in:fade>
+        <RetroIntroView />
+    </div>
 </main>
 
 <style>
@@ -73,31 +40,5 @@
         display: flex;
         justify-content: center;
         align-items: center;
-    }
-
-    .dashboard-layout {
-        width: 100%;
-        height: 100vh; /* Restored fixed height */
-        display: flex;
-    }
-
-    .center-stage {
-        flex: 1;
-        height: 100%; /* Match parent */
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        z-index: 1;
-    }
-
-    .view-container {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 2rem;
-        box-sizing: border-box;
     }
 </style>
